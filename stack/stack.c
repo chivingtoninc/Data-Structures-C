c92c2af1e403a95df8fd774094b071e2d1a6c770
Johnathan Chivington <chivingtoninc@gmail.com>
2019-02-01 06:33:51 -0800

    Adding stack methods and main/driver.

--- stack/stack.c
+++ stack/stack.c
@@ -5,9 +5,13 @@
  * License: DO_WHATEVER_YOU_WANT - use/modify/redistribute as you like.              *
  * --------------------------------------------------------------------------------- */
 
+
+/* ------------------------------------ Includes ----------------------------------- */
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* ----------------------------------- Structures ---------------------------------- */
 typedef struct StackNode {
   //
 } StackNode;
@@ -15,3 +19,28 @@ typedef struct StackNode {
 typedef struct Stack {
   //
 } Stack;
+
+
+/* ------------------------------------ Methods ------------------------------------ */
+int push(Stack* stack, int value) {
+  // push onto stack & return stack
+}
+
+int pop(Stack* stack) {
+  // push onto stack & return stack
+}
+
+int peek(Stack* stack) {
+  // push onto stack & return stack
+}
+
+int empty(Stack* stack) {
+  // push onto stack & return stack
+}
+
+
+/* ------------------------------------- Main -------------------------------------- */
+int main(int argc, char const *argv[]) {
+  // driver code
+  return 0;
+}
