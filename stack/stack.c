0732811ee4c7f6d291636db899c75aa2a2b284e5
Johnathan Chivington <chivingtoninc@gmail.com>
2019-02-01 06:53:43 -0800

    Defining Stack structures & method i/o.

--- stack/stack.c
+++ stack/stack.c
@@ -9,33 +9,44 @@
 /* ------------------------------------ Includes ----------------------------------- */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /* ----------------------------------- Structures ---------------------------------- */
 typedef struct StackNode {
-  //
+  int value;                  // the value that the node holds
+  StackNode* next;            // pointer to the next node in the stack
 } StackNode;
 
 typedef struct Stack {
-  //
+  size_t size;                // size of the stack
+  StackNode* top;             // pointer to top node in stack
 } Stack;
 
 
 /* ------------------------------------ Methods ------------------------------------ */
-int push(Stack* stack, int value) {
-  // push onto stack & return stack
+Stack* push(Stack* stack, int value) {
+  // in: stack pointer & value to place on stack
+  // out: stack pointer with additional StackNode
+  // err: (-1 = general error)
 }
 
-int pop(Stack* stack) {
-  // push onto stack & return stack
+Stack* pop(Stack* stack) {
+  // in: stack pointer
+  // out: stack pointer with top StackNode removed
+  // err: (-1 = general error)
 }
 
 int peek(Stack* stack) {
-  // push onto stack & return stack
+  // in: stack pointer
+  // out: top value of stack
+  // err: (-1 = general error)
 }
 
 int empty(Stack* stack) {
-  // push onto stack & return stack
+  // in: stack pointer
+  // out: (1 = empty | 0 = not empty)
+  // err: (-1 = general error)
 }
 
 
